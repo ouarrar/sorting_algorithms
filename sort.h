ba98088166ca9241ec4e087eb794757f8b395baa
ouarrar abdelouahed <ouarrar.abdelouahed@gmail.com>
2023-11-21 12:16:25 -0800

    task0

--- sort.h
+++ sort.h
@@ -4,6 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
